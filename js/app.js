31c14fe9095b40b96501a755957ea0813c98be77
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-20 18:13:50 -0600

    With app.js, add while and if loops to test for fizz, buzz, fizz buzz or the number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 // app.js
+var counter = 1;
+
+while (counter <= 100) {
+    if ((counter % 3) === 0) {
+        if ((counter % 5) === 0) {
+            console.log('fizz buzz');
+        } else {
+            console.log('fizz');
+        }
+    } else if ((counter % 5) === 0) {
+        console.log('buzz');
+    } else {
+        console.log(counter);
+    }
+    counter++;
+}
