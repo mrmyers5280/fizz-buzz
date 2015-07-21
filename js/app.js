76174c7c88769d5a41614dcf2cfa457193bc131b
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-20 19:38:55 -0600

    With app.js, add document ready

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 // app.js
-var counter = 1;
+$(document).ready(function(){
+    var counter = 1;
 
-while (counter <= 100) {
-    if ((counter % 3) === 0) {
-        if ((counter % 5) === 0) {
-            console.log('fizz buzz');
+    while (counter <= 100) {
+        if ((counter % 3) === 0) {
+            if ((counter % 5) === 0) {
+                console.log('fizz buzz');
+                $('body').append('fizz buzz<br />');
+            } else {
+                console.log('fizz');
+                $('body').append('fizz<br />');
+            }
+        } else if ((counter % 5) === 0) {
+            console.log('buzz');
+            $('body').append('buzz<br />');
         } else {
-            console.log('fizz');
+            console.log(counter);
+            $('body').append(counter + '<br />');
         }
-    } else if ((counter % 5) === 0) {
-        console.log('buzz');
-    } else {
-        console.log(counter);
+        counter++;
     }
-    counter++;
-}
+});
