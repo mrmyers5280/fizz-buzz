9222a1f7693dd8fa960963679efabe4b3106e2dd
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-20 19:45:07 -0600

    With app.js, comment out console.logs

--- js/app.js
+++ js/app.js
@@ -5,17 +5,17 @@ $(document).ready(function(){
     while (counter <= 100) {
         if ((counter % 3) === 0) {
             if ((counter % 5) === 0) {
-                console.log('fizz buzz');
+                //console.log('fizz buzz');
                 $('body').append('fizz buzz<br />');
             } else {
-                console.log('fizz');
+                //console.log('fizz');
                 $('body').append('fizz<br />');
             }
         } else if ((counter % 5) === 0) {
-            console.log('buzz');
+            //console.log('buzz');
             $('body').append('buzz<br />');
         } else {
-            console.log(counter);
+            //console.log(counter);
             $('body').append(counter + '<br />');
         }
         counter++;
