6ea5971cb37cedae0ab87e41799c373ace11e985
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-23 09:05:37 -0600

    With app.js, refactor with for loop rather than while loop and wrapped in a function

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 // app.js
 $(document).ready(function(){
-    var counter = 1;
 
-    while (counter <= 100) {
-        if ((counter % 3) === 0) {
-            if ((counter % 5) === 0) {
-                //console.log('fizz buzz');
-                $('body').append('fizz buzz<br />');
+    function fizzbuzz(max) {
+        for (var i = 1; i <= max; i++) {
+            if ((i % 3) === 0) {
+                if ((i % 5) === 0) {
+                    console.log('fizz buzz');
+                    $('body').append('fizz buzz<br />');
+                } else {
+                    console.log('fizz');
+                    $('body').append('fizz<br />');
+                }
+            } else if ((i % 5) === 0) {
+                console.log('buzz');
+                $('body').append('buzz<br />');
             } else {
-                //console.log('fizz');
-                $('body').append('fizz<br />');
+                console.log(i);
+                $('body').append(i + '<br />');
             }
-        } else if ((counter % 5) === 0) {
-            //console.log('buzz');
-            $('body').append('buzz<br />');
-        } else {
-            //console.log(counter);
-            $('body').append(counter + '<br />');
         }
-        counter++;
-    }
+    };
+    fizzbuzz(15);
 });
+// get max value from user. Check that it's an integer.
+// function fizzbuzz(max) {
+//     //for loop until max reached
+//     for (var i = 0; i <= max; i++) {
+//         var result = '';
+//         if ((i % 3) === 0) {
+//             result = 'fizz';
+//         }
+//     }
+// }
