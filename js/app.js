d9fa746da262ca6aeb832da658cddaf1f80f262b
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-23 09:24:00 -0600

    With app.js, refactor print statements to use variable and print once

--- js/app.js
+++ js/app.js
@@ -2,31 +2,24 @@
 $(document).ready(function(){
 
     function fizzbuzz(max) {
+        var result = '';
         for (var i = 1; i <= max; i++) {
-            if ((i % 3) === 0 && (i % 5) === 0) {
+            if ((i % 3 === 0) && (i % 5 === 0)) {
                 console.log('fizzbuzz');
-                $('body').append('fizzbuzz<br />');
-            } else if ((i % 3) === 0) {
+                result = 'fizzbuzz<br />';
+            } else if (i % 3 === 0) {
                 console.log('fizz');
-                $('body').append('fizz<br />');
-            } else if ((i % 5) === 0) {
+                result = 'fizz<br />';
+            } else if (i % 5 === 0) {
                 console.log('buzz');
-                $('body').append('buzz<br />');
+                result = 'buzz<br />';
             } else {
                 console.log(i);
-                $('body').append(i + '<br />');
+                result = i + '<br />';
             }
+            $('body').append(result);
         }
     };
     fizzbuzz(15);
 });
 // get max value from user. Check that it's an integer.
-// function fizzbuzz(max) {
-//     //for loop until max reached
-//     for (var i = 0; i <= max; i++) {
-//         var result = '';
-//         if ((i % 3) === 0) {
-//             result = 'fizz';
-//         }
-//     }
-// }
