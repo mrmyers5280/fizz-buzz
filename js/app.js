65f495a284164ad948aaddcda7e0bf153ec0e199
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-07-24 11:05:05 -0700

    With app.js, add test for NaN and comment console.log calls

--- js/app.js
+++ js/app.js
@@ -2,32 +2,34 @@
 $(document).ready(function(){
 
     $('body').on('click', '#fizzbuzz-button', function() {
-        console.log('fizzbuzz-button clicked');
+        // console.log('fizzbuzz-button clicked');
         var maximum = parseInt($('#fizzbuzz-max').val());
         // console.log(maximum + 10);
-        fizzbuzz(maximum);
+        if (isNaN(maximum)) {
+            alert('Please enter a whole number for the maiximum value.')
+        } else {
+            fizzbuzz(maximum);
+        }
     });
 
     function fizzbuzz(max) {
         var result = '';
         for (var i = 1; i <= max; i++) {
             if ((i % 3 === 0) && (i % 5 === 0)) {
-                console.log('fizzbuzz');
+                // console.log('fizzbuzz');
                 result = 'fizzbuzz<br />';
             } else if (i % 3 === 0) {
-                console.log('fizz');
+                // console.log('fizz');
                 result = 'fizz<br />';
             } else if (i % 5 === 0) {
-                console.log('buzz');
+                // console.log('buzz');
                 result = 'buzz<br />';
             } else {
-                console.log(i);
+                // console.log(i);
                 result = i + '<br />';
             }
             $('body').append(result);
         }
     }
 
-    //fizzbuzz(15);
 });
-// get max value from user. Check that it's an integer.
